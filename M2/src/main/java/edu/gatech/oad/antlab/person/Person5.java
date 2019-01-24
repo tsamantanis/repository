df7b8980bfb495e4c7c2ed8f88a58f8b089d9828
mmossien6 <mmossien6@gatech.edu>
2019-01-23 21:05:13 -0500

    added name to antlabmain

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -30,8 +30,13 @@ public class Person5 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 5 put your implementation here
-	  return null;
+		if (input.length() <= 2) {
+			return input;
+		}
+		String rotated = "";
+		rotated += input.substring(2, input.length());
+		rotated += input.substring(0, 2);
+		return rotated;
 	}
 	
 	/**
